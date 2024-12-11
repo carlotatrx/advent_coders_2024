4d71451762ac0542a89410dc3e4d0c73007ec8f8
Corbella Alcantara <ccorbella@giub.local@climcal4.giub.unibe.ch>
2024-12-11 22:00:04 +0100

    create id number thingy

--- carlota/day09.py
+++ carlota/day09.py
@@ -12,21 +12,25 @@ with open('/scratch2/ccorbella/code/adventcode2024/carlota/day09_input.txt') as 
 #%%
 
 updated_data = list()
-
+start_with = 0
 for i, letter in enumerate(data):
     if i%2 != 0: # odd positions are points
-        updated_data.append('.'*i)
+        updated_data.append('.'*int(letter))
     else:
-        updated_data.append(str(i)*i)
+        updated_data.append(str(start_with)*int(letter))
+        start_with += 1
 
-updated_data = updated_data[1:]
+updated_data = [item for item in updated_data if item != ''] # remove empty strings
+
 
 str_data = str()
+start_with = 0
 for i, letter in enumerate(data):
     if i%2 != 0: # odd positions are points
-        str_data += '.'*i
+        str_data += '.'*int(letter)
     else:
-        str_data += str(i)*i
+        str_data += str(start_with)*int(letter)
+        start_with += 1
         
 with open('/scratch2/ccorbella/code/adventcode2024/carlota/day09_str.txt', 'w') as file:
     file.write(str_data)
@@ -42,3 +46,6 @@ while(str_data):
         
         str_data[pos_now] = str_data[end_pos]
         str_data = str_data[:-1]
+        
+with open('/scratch2/ccorbella/code/adventcode2024/carlota/day09_strfinal.txt', 'w') as file:
+    file.write(str_data)
